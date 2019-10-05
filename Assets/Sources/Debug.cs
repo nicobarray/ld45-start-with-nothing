8bc3146ad491d9b10fbfdc5a017390c2235bf179
Nicolas Barray <nbarray@gmail.com>
2019-10-05 14:21:36 +0200

    Add island generator + plain sprite

---
+++ Assets/Sources/Debug.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LDJAM45
+{
+    public class Debug : MonoBehaviour
+    {
+        public GameObject unitPrefab;
+
+        void Start()
+        {
+            int width = 1280;
+            int height = 720;
+            int ppu = 64;
+
+            for (int i = 0; i < width / ppu; i++)
+            {
+                for (int j = 0; j < height / ppu; j++)
+                {
+                    GameObject gameObject = Instantiate(unitPrefab, transform);
+                    print(i + ' ' + j);
+                    gameObject.transform.position = new Vector2(i, j);
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(256, 256, i + j / 250);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Sources/IslandGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LDJAM45
+{
+    enum IslandSlab
+    {
+        BEACH,
+        PLAIN,
+        FOREST,
+        CAMP
+    }
+
+    public class IslandGenerator : MonoBehaviour
+    {
+        [Header("Slab prefabs")]
+        public GameObject beachPrefab;
+        public GameObject plainPrefab;
+        public GameObject forestPrefab;
+
+        private struct Slab
+        {
+            public GameObject gameObject;
+            public IslandSlab type;
+        }
+
+        private Slab[] islandSlabs = new Slab[7];
+
+        void Start()
+        {
+            for (int i = 0; i < islandSlabs.Length; ++i)
+            {
+                IslandSlab type = IslandSlab.PLAIN;
+                GameObject slabPrefab = plainPrefab;
+                if (i == islandSlabs.Length / 2)
+                {
+                    type = IslandSlab.CAMP;
+                    slabPrefab = plainPrefab;
+                }
+                else if (i == 0 || i == islandSlabs.Length - 1)
+                {
+                    type = IslandSlab.BEACH;
+                    slabPrefab = beachPrefab;
+                }
+                else if (UnityEngine.Random.value < 0.5)
+                {
+                    type = IslandSlab.FOREST;
+                    slabPrefab = forestPrefab;
+                }
+
+                GameObject slabGameObject = Instantiate(slabPrefab, transform);
+                slabGameObject.name = "Slab_" + i + "_" + type.ToString();
+                // ? Each slab is 20 unit of width.
+                slabGameObject.transform.position = new Vector3((i - islandSlabs.Length / 2) * 20, 0);
+
+                islandSlabs[i] = new Slab
+                {
+                    gameObject = slabGameObject,
+                    type = type
+                };
+            }
+        }
+    }
+}
