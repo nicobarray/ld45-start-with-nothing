592b873c3cdc9e0dd5c739e2b646b59181a67c05
Nicolas Barray <nbarray@gmail.com>
2019-10-05 15:51:02 +0200

    Add player camera and movements

---
+++ Assets/Sources/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Components")]
+    public Rigidbody2D rb;
+
+    [Header("Player Configuration")]
+    public float speed = 5;
+
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput == 0)
+        {
+            return;
+        }
+
+        transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
+    }
+}
